d385e92ffcdf42b6c1019817d43bd67df31eeca5
wangsoap <48651361+wangsoap@users.noreply.github.com>
2019-06-12 22:24:30 +0800

    Update cord.cpp

--- labs/lab2/cord.cpp
+++ labs/lab2/cord.cpp
@@ -48,20 +48,28 @@ cord_t cord_join(cord_t R, cord_t S) {
 }
 
 string cord_tostring(cord_t R) {
-    // TODO: Your implementation here
-    return "";
+    if (R== nullptr) return "";
+    if (R->left == nullptr && R->right == nullptr) return R->data;
+    return cord_tostring(R->left)+cord_tostring(R->right);
 }
 
 char cord_charat(cord_t R, size_t i)
 /*@requires 0 <= i && i < cord_length(R); @*/
 {
-    // TODO: Your implementation here
-    return '\0';
+    if (R== nullptr) return '\0';
+    if (R->left == nullptr && R->right == nullptr) return R->data[i];
+    if (i<R->left->len) return cord_charat(R->left,i);
+    else return cord_charat(R->right,i-R->left->len);
 }
 
 cord_t cord_sub(cord_t R, size_t lo, size_t hi)
 /*@requires 0 <= lo && lo <= hi && hi <= cord_length(R); @*/
 {
-    // TODO: Your implementation here
-    return nullptr;
+    if (lo==hi) return cord_new("");
+    if (lo==0 && hi==R->len) return R;
+    if (R->left == nullptr && R->right == nullptr) return cord_new(R->data.substr(lo,hi-lo));
+
+    if (lo<R->left->len && hi<=R->left->len) {return cord_sub(R->left,lo,hi);}
+    else if (lo>=R->left->len && hi>R->left->len) {return cord_sub(R->right,lo-R->left->len,hi-R->left->len);}
+    else return cord_join(cord_sub(R->left,lo,R->left->len),cord_sub(R->right,0,hi-R->left->len));
 }
