d9ff527fcd29c78ae962cd6e11d894caccd3eef6
wangsoap <48651361+wangsoap@users.noreply.github.com>
2019-07-10 15:07:45 +0800

    Update grid.cpp

--- labs/lab3/grid.cpp
+++ labs/lab3/grid.cpp
@@ -52,22 +52,77 @@ bool Grid::isEmpty(const Point &pt) const {
 }
 
 void Grid::upgradeTile(const Point &pt) {
-    // TODO: Your implementation here
+    //this->setSquare(pt,getSquare(pt)+1);
+    //I know it is wrong but I don't know what's happening here even with the hint.
 }
 
 unsigned int Grid::collapseTiles(Direction dir) {
-    // TODO: Your implementation here
-    return 0;
+    unsigned int points=0;
+    if (dir==UP || dir==DOWN){
+        for (int i = 0; i < height; i++) {
+            if (insideGrid(adjacentPoint({i, 0}, dir))) {
+                for (int j = 0; j < width; j++) {
+                    if (getSquare({i,j})==getSquare(adjacentPoint({i, j}, dir))) {
+                        clearSquare({i,j});
+                        upgradeTile(adjacentPoint({i, j}, dir));
+                        points+=getSquare(adjacentPoint({i, j}, dir))->points;
+                    }
+                }
+            }
+        }
+    }else {
+        for (int j = 0; j < width; j++) {
+            if (insideGrid(adjacentPoint({0, j}, dir))) {
+                for (int i = 0; i < height; i++) {
+                    if (getSquare({i,j})==getSquare(adjacentPoint({i, j}, dir))) {
+                        clearSquare({i,j});
+                        upgradeTile(adjacentPoint({i, j}, dir));
+                        points+=getSquare(adjacentPoint({i, j}, dir))->points;
+                    }
+                }
+            }
+        }
+    }
+    return points;
 }
-
 bool Grid::shiftTile(const Point &dst, const Point &src) {
-    // TODO: Your implementation here
+    if (!isEmpty(src) && isEmpty(dst)) {
+        setSquare(dst,getSquare(src));
+        setSquare(src, nullptr);
+        return true;
+    }
     return false;
 }
 
 bool Grid::shiftTiles(Direction dir) {
-    // TODO: Your implementation here
-    return false;
+    bool change= false,all= false;
+    while (!change) {
+        change=true;
+        if (dir == UP || dir == DOWN) {
+            for (int i = 0; i < height; i++) {
+                if (insideGrid(adjacentPoint({i, 0}, dir))) {
+                    for (int j = 0; j < width; j++) {
+                        while (shiftTile(adjacentPoint({i, j}, dir), {i, j})) {
+                            all = true;
+                            change= false;
+                        }
+                    }
+                }
+            }
+        } else {
+            for (int j = 0; j < width; j++) {
+                if (insideGrid(adjacentPoint({0, j}, dir))) {
+                    for (int i = 0; i < height; i++) {
+                        while (shiftTile(adjacentPoint({i, j}, dir), {i, j})) {
+                            change= false;
+                            all = true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return all;
 }
 
 void Grid::printGrid() const {
